4a16f165260ad9ae4f710643bf1134ce5b8b6db3
Andrew Zarei <andrew.zarei.09@gmail.com>
2024-04-18 17:37:38 +0100

    added create and delete functions

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -17,6 +17,16 @@ class AlbumRepository():
         row = row[0]
         return Album(row["id"], row["title"], row["release_year"], row["artist_id"])
     
+    def create(self, album):
+        self._connection.execute('INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)', 
+                                 [album.title, album.year, album.artist_id])
+        return None
+    
+    def delete(self, album_id):
+        self._connection.execute('DELETE FROM albums WHERE id = %s', [album_id])
+        return None
+    
+    
     
 
     
